191fe959f50cb54738ac19990b786ccfb37dd2d6
Giancarlo Lelli <gcarlo.lelli@live.com>
2017-02-04 20:27:34 +0100

    adds the http client and basic auth mechanism

---
+++ src/Microsoft.SDK.Groove.Client/GrooveMusicClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SDK.Groove.Client.Http;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Microsoft.SDK.Groove.Client
+{
+    public sealed class GrooveMusicClient
+    {
+        private readonly string m_clientId;
+        private readonly string m_secret;
+        private readonly RegionInfo m_culture;
+        private readonly Lazy<GrooveHttpClient> m_client;
+
+        public GrooveMusicClient(string clientId, string clientSecret)
+        {
+            m_clientId = clientId;
+            m_secret = clientSecret;
+            m_culture = new RegionInfo("us");
+            m_client = new Lazy<GrooveHttpClient>(() => new GrooveHttpClient(m_clientId, m_secret));
+        }
+
+        public GrooveMusicClient(string clientId, string clientSecret, RegionInfo locale)
+        {
+            m_culture = locale;
+            m_clientId = clientId;
+            m_secret = clientSecret;
+            m_client = new Lazy<GrooveHttpClient>(() => new GrooveHttpClient(m_clientId, m_secret));
+        }
+    }
+}
---
+++ src/Microsoft.SDK.Groove.Client/Http/GrooveHttpClient.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.SDK.Groove.Extensions;
+using Microsoft.SDK.Groove.Models.Authentication;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.SDK.Groove.Client.Http
+{
+    public class GrooveHttpClient : IDisposable
+    {
+        private readonly HttpClient m_client;
+        private readonly Dictionary<string, string> m_requestData;
+
+        public AuthTokenModel AccessToken { get; private set; }
+
+        public GrooveHttpClient(string clientId, string clientSecret)
+        {
+            m_client = new HttpClient();
+            m_client.BaseAddress = new Uri("https://music.xboxlive.com/1/content/music/");
+
+            m_requestData = new Dictionary<string, string>()
+            {
+                { "client_id", clientId },
+                { "client_secret", clientSecret },
+                { "scope", "http://music.xboxlive.com" },
+                { "grant_type", "client_credentials" }
+            };
+
+            Task.Run(async () => await AuthenticateAsync()).Wait();
+        }
+
+        public async Task<T> QueryServiceAsync<T>(string request)
+        {
+            await ValidateTokenAsync();
+            request = string.Concat(request, $"&accessToken=Bearer+{AccessToken.AccessToken}");
+            var response = await m_client.GetAsync(request);
+            string json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private async Task AuthenticateAsync()
+        {
+            var response = await m_client.PostAsync(new Uri("https://datamarket.accesscontrol.windows.net/v2/OAuth2-13"), new FormUrlEncodedContent(m_requestData));
+            if (response.IsSuccessStatusCode)
+            {
+                String responseString = await response.Content.ReadAsStringAsync();
+                AuthTokenModel tokenResponse = JsonConvert.DeserializeObject<AuthTokenModel>(responseString);
+                AccessToken = tokenResponse;
+                AccessToken.TokenExpiryDate = Convert.ToDouble(tokenResponse.ExpiresIn).UnixTimeStampToDateTime();
+            }
+        }
+
+        private async Task ValidateTokenAsync()
+        {
+            if (DateTime.UtcNow > AccessToken.TokenExpiryDate)
+                await AuthenticateAsync();
+        }
+
+        public void Dispose() => m_client?.Dispose();
+    }
+}
